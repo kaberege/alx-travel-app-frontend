918397c00796518066eddbd0fe6311c9d7002c8f
kaberege <nestor.godard.kaberege@gmail.com>
2026-05-06 12:22:53 +0200

    refactor: update API client to use cookies for auth

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api",
@@ -8,7 +9,7 @@ export const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = Cookies.get("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -19,7 +20,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
+      Cookies.remove("token");
       window.location.href = "/login";
     }
     return Promise.reject(error);
